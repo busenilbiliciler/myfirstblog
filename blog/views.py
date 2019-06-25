df4cdb036387c4454bbfd2ea9bf393eb1f154022
busenilbiliciler <bnbiliciler44@gmail.com>
2019-06-25 16:43:38 +0300

    taze değiştirildi

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
-
-from django.shortcuts import render
+from __future__ import unicode_literals
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render
 
 
 def post_list(request):
 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+
